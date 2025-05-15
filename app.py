d0b9716847531d9cb391ae87d2ff5f6600186e93
Dymitri <dymitrif@vivaldi.net>
2025-05-15 13:53:54 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+# Rota principal
+@app.route('/')
+def home():
+    return render_template('interface_chopp.html')
+
+# Rota simulada para retornar saldo
+@app.route('/api/saldo')
+def saldo():
+    return jsonify({
+        'saldo': 50.00,
+        'pendente': 0.00,
+        'cartao': '5432',
+        'status': 'ativo'
+    })
+
+# Rota simulada para processar pagamento de consumo
+@app.route('/api/pagar-consumo')
+def pagar_consumo():
+    return jsonify({'status': 'pago', 'mensagem': 'Consumo pendente pago com sucesso.'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
